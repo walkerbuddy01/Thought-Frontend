86779f9308eb9f229a239afe9880f14d48ac7c6d
walkerbuddy01 <136956295+walkerbuddy01@users.noreply.github.com>
2024-04-30 16:38:01 +0530

    Home page updation

--- src/Components/DockNavigation.tsx
+++ src/Components/DockNavigation.tsx
@@ -4,14 +4,20 @@ import { GoHome } from "react-icons/go";
 import { Link } from "react-router-dom";
 import { CiUser } from "react-icons/ci";
 
-function DockNavigation(): JSX.Element {
+interface DockNavigationProps {
+  className?: string;
+}
+
+function DockNavigation({ className }: DockNavigationProps): JSX.Element {
   const Icons = {
     createBlog: () => <IoCreateOutline size={"1.5em"} />,
     home: () => <GoHome size={"1.5em"} />,
     profile: () => <CiUser size={"1.5em"} />,
   };
   return (
-    <div className=" flex h-[80px] w-full flex-cols items-center justify-center overflow-hidden rounded-lg  ">
+    <div
+      className={`${className} flex h-[80px] w-full flex-cols items-center justify-center overflow-hidden rounded-lg  `}
+    >
       <Dock>
         <DockIcon>
           <Link to={"/blog"}>
